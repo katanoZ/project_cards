bb7e27a53b5b1219acf601b8073eac79e204576b
katanoZ <katano9@yahoo.co.jp>
2019-05-21 22:26:30 +0900

    [add]staging環境にbasic認証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  if Rails.env.staging?
+    http_basic_authenticate_with name: ENV.fetch('BASIC_AUTH_USERNAME'),
+                                 password: ENV.fetch('BASIC_AUTH_PASSWORD')
+  end
 end
