60eb4668a9d41edab94f90e12743c9fb37dc8b17
katanoZ <katano9@yahoo.co.jp>
2019-06-01 00:05:38 +0900

    [add]ファイルサイズバリデータのテストを作成

---
+++ spec/validators/file_size_validator_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe FileSizeValidator, type: :validator do
+  let(:model_class) do
+    Struct.new(:file) do
+      include ActiveModel::Validations
+      validates :file, file_size: { maximum: 1.kilobytes }
+    end
+  end
+
+  describe '#validate_each' do
+    let(:model) { model_class.new(file) }
+
+    context 'ファイルサイズが最大値以下の場合' do
+      let(:file) { fixture_file_upload('sample.png', 'image/png') }
+
+      it '結果が正しいこと' do
+        expect(model).to be_valid
+      end
+    end
+
+    context 'ファイルサイズが最大値以上の場合' do
+      let(:file) { fixture_file_upload('sample.pdf', 'application/pdf') }
+
+      it '結果が正しいこと' do
+        expect(model).to be_invalid
+      end
+
+      it 'エラーメッセージが正しいこと' do
+        model.validate
+        expect(model.errors[:file]).to eq ['のファイルサイズは1KB以下にしてください']
+      end
+    end
+  end
+end
