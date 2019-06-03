c1aff64a2b56bcf0efa3bcc4ca2283a124a6636f
katanoZ <katano9@yahoo.co.jp>
2019-06-03 20:11:52 +0900

    [add]マイページにファイル選択時の画面表示効果jsを追加

--- frontend/javascripts/application.js
+++ frontend/javascripts/application.js
@@ -1,2 +1,5 @@
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all';
+import './set_mypage_filename';
+import './set_mypage_preview';
+
---
+++ frontend/javascripts/set_mypage_filename.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', setMyPageFileName);
+
+//ファイル選択時に、ファイル選択ボタンにファイル名を表示
+function setMyPageFileName(){
+  const myPageFileField = document.querySelector('#js-mypage_filefield');
+
+  myPageFileField.addEventListener('change', () => {
+    const inputFileName = document.querySelector('#js-mypage_filefield').files[0].name;
+    const fileNameText = document.querySelector('#js-mypage_filename_text');
+    fileNameText.textContent = inputFileName;
+  });
+};
