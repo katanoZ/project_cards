2be2b226e6d13d2c39b5bd185699c43026ca4c63
katanoZ <katano9@yahoo.co.jp>
2019-06-04 11:52:45 +0900

    [add]マイページにログアウトボタンを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'projects#index'
 
+  # ログインページ
   get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
   get 'auth/:provider/callback', to: 'sessions#create'
 
   # マイページ
