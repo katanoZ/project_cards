1e9e8389c4b135724c5377a13614ec25994e91df
katanoZ <katano9@yahoo.co.jp>
2019-06-07 03:58:22 +0900

    [add]マイプロジェクト一覧にプロジェクト作成リンクを追加

--- app/controllers/myprojects/projects_controller.rb
+++ app/controllers/myprojects/projects_controller.rb
@@ -1,6 +1,9 @@
 class Myprojects::ProjectsController < ApplicationController
   def index
     @projects = Project.where(owner: current_user).order(id: :desc).page(params[:page]).per(9)
-    render 'projects/index'
+    respond_to do |format|
+      format.html
+      format.js { render 'projects/index' }
+    end
   end
 end
