06115630341e526e76e09d61879683e1f2f8c3d5
katanoZ <katano9@yahoo.co.jp>
2019-06-07 07:28:22 +0900

    [clean]定数にコメントを追記

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,9 @@
 module Constants
   # アプリケーション全体に関する定数はここに書く
   # （model全体に関する定数はapp/models/application_record.rbに書く）
+  # （個々のmodelに関する定数はそのmodelに書く）
+
+  # Content-Typeに対して表示する名称
   CONTENT_TYPE_NAMES = {
     'image/jpeg': 'JPEG',
     'image/jpg': 'JPEG',
