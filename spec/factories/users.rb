0ada54cadeede67cbb0683296ef94f490af2ee22
katanoZ <katano9@yahoo.co.jp>
2019-06-07 17:04:29 +0900

    [fix]userファクトリを修正
    Fakerの生成する値がランダムではなく固定になっていたため、ランダムになるよう修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,7 @@
 FactoryBot.define do
-  factory :user do
-    auth = [
-      Faker::Omniauth.unique.google,
-      Faker::Omniauth.unique.twitter,
-      Faker::Omniauth.unique.github
-    ].sample
-
-    provider { auth[:provider] }
-    uid { auth[:uid] }
-    name { auth[:info][:name] }
+  factory :user, aliases: [:owner] do
+    provider { Faker::Omniauth.unique.google[:provider] }
+    uid { Faker::Omniauth.unique.google[:uid] }
+    name { Faker::Omniauth.unique.google[:info][:name] }
   end
 end
