f9b183a6b0ee1c59a59198d891ca379332e19c34
katanoZ <katano9@yahoo.co.jp>
2019-06-10 14:44:42 +0900

    [clean]form helperのテストを修正

--- spec/helpers/form_helper_spec.rb
+++ spec/helpers/form_helper_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe FormHelper, type: :helper do
     subject { field_class(model: model, field: field) }
 
     let(:model) { double('test_model') }
+    let(:field) { :test_field }
     before do
-      messages = { invalid_field: ['error'], valid_field: [] }
       allow(model).to receive(:errors).and_return(messages)
     end
 
     context '項目にエラーがある場合' do
-      let(:field) { :invalid_field }
+      let(:messages) { { test_field: ['error'] } }
 
       it '内容が正しいこと' do
         is_expected.to eq 'form-control form-control-lg is-invalid'
@@ -19,7 +19,7 @@ RSpec.describe FormHelper, type: :helper do
     end
 
     context '項目にエラーがない場合' do
-      let(:field) { :valid_field }
+      let(:messages) { { test_field: [] } }
 
       it '内容が正しいこと' do
         is_expected.to eq 'form-control form-control-lg'
