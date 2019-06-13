0af9b2d07d9251c0a56763d0b1691a142b94419b
katanoZ <katano9@yahoo.co.jp>
2019-06-14 07:26:42 +0900

    [add]cardモデルにバリデーション作成

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,8 @@ class Card < ApplicationRecord
   belongs_to :assignee, class_name: 'User', foreign_key: :assignee_id
   belongs_to :project
   belongs_to :column
+
+  validates :name, presence: true,
+                   uniqueness: { scope: :project },
+                   length: { maximum: 40 }
 end
