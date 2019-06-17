7df097fb6145cd0bec40765aa8ab3965f7f11194
katanoZ <katano9@yahoo.co.jp>
2019-06-17 12:24:02 +0900

    [fix]画像が表示されないケースの対応
    omniauthで取得したinfo.imageのファイル名がcontent_typeと異なる拡張子の場合があるため、
    cloudinaryでエラーにならないように、content_typeに合わせた拡張子で保存するよう修正した。

--- app/models/concerns/remote_file_attachable.rb
+++ app/models/concerns/remote_file_attachable.rb
@@ -3,12 +3,21 @@ require 'open-uri'
 module RemoteFileAttachable
   def attach_remote_file!(url)
     OpenURI.open_uri(url) do |file|
+      content_type = file.content_type
       attachment_target.attach(
         io: file,
-        filename: File.basename(url),
-        content_type: file.content_type
+        filename: filename(url, content_type),
+        content_type: content_type
       )
     end
+  end
+
+  def filename(url, content_type)
+    # omniauthで取得したinfo.imageのファイル名がcontent_typeと異なる拡張子の場合があるため、
+    # cloudinaryでエラーにならないように、content_typeに合わせた拡張子を付け直す。
+    name = File.basename(url, '.*')
+    ext = Rack::Mime::MIME_TYPES.invert[content_type]
+    "#{name}#{ext}"
   end
 
   def attachment_target
