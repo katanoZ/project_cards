ca5e05851414efb2212de5aa9454e996733f9638
katanoZ <katano9@yahoo.co.jp>
2019-06-22 14:31:02 +0900

    [add]プロジェクト参加時に招待を削除する機能を追加

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -4,4 +4,16 @@ class Participation < ApplicationRecord
 
   validates :user, uniqueness: { scope: :project }
   validates :user, owner_rejection: true
+
+  before_create :destroy_invitation!, if: :invited?
+
+  def invited?
+    user.invited?(project)
+  end
+
+  private
+
+  def destroy_invitation!
+    user.invitations.find_by!(project_id: project.id).destroy!
+  end
 end
