011d2c589c841711ccccd00548036fdd79e156eb
katanoZ <katano9@yahoo.co.jp>
2019-07-04 16:07:57 +0900

    [update]ログ用コールバッククラスのテストを修正
    モデルをcreateする際に#run_callbacks(:create)を使っているのは、（#saveを除く）#createのコールバックのみを検証対象にしたいため。

--- spec/callbacks/column_create_logs_callbacks_spec.rb
+++ spec/callbacks/column_create_logs_callbacks_spec.rb
@@ -1,33 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe ColumnCreateLogsCallbacks, type: :callback do
-  let(:user) { create(:user, name: '山田太郎') }
-  let(:project) { create(:project, owner: user) }
+  let!(:user) { create(:user, name: '山田太郎') }
+  let!(:project) { create(:project, owner: user) }
   let(:column) { build(:column, name: 'テスト', project: project) }
 
   describe '#after_create' do
-    it '結果が正しいこと' do
-      expect { column.save }
-        .to change { Log.where('content LIKE(?)', '%カラム%作成%').count }.by(1)
-    end
-  end
-
-  describe '#content' do
     context '操作者が設定されている場合' do
       before { column.operator = user }
 
-      it '内容が正しいこと' do
-        column.save
-        log = Log.where('content LIKE(?)', '%カラム%作成%').last
+      it 'ログが1件作成されること' do
+        expect { column.run_callbacks(:create) }.to change { Log.count }.by(1)
+      end
+
+      it 'ログの内容が正しいこと' do
+        column.run_callbacks(:create)
+        log = Log.order(:created_at).last
         expect(log.content).to eq '山田太郎さんがテストカラムを作成しました'
         expect(log.project).to eq project
       end
     end
 
     context '操作者が設定されていない場合' do
-      it '内容が正しいこと' do
-        column.save
-        log = Log.where('content LIKE(?)', '%カラム%作成%').last
+      it 'ログが1件作成されること' do
+        expect { column.run_callbacks(:create) }.to change { Log.count }.by(1)
+      end
+
+      it 'ログの内容が正しいこと' do
+        column.run_callbacks(:create)
+        log = Log.order(:created_at).last
         expect(log.content).to eq 'テストカラムが作成されました'
         expect(log.project).to eq project
       end
--- spec/callbacks/participation_create_logs_callbacks_spec.rb
+++ spec/callbacks/participation_create_logs_callbacks_spec.rb
@@ -1,20 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ParticipationCreateLogsCallbacks, type: :callback do
-  let(:project) { create(:project) }
-  let(:user) { create(:user, name: '佐藤花子') }
+  let!(:project) { create(:project) }
+  let!(:user) { create(:user, name: '佐藤花子') }
 
   describe '#after_create' do
-    it '結果が正しいこと' do
-      expect { user.participate_in(project) }
-        .to change { Log.where('content LIKE(?)', '%参加%').count }.by(1)
+    it 'ログが1件作成されること' do
+      expect { user.participate_in(project) }.to change { Log.count }.by(1)
     end
-  end
 
-  describe '#content' do
-    it '内容が正しいこと' do
+    it 'ログの内容が正しいこと' do
       user.participate_in(project)
-      log = Log.where('content LIKE(?)', '%参加%').last
+      log = Log.order(:created_at).last
       expect(log.content).to eq '佐藤花子さんがこのプロジェクトに参加しました'
       expect(log.project).to eq project
     end
